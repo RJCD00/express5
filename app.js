00e4dad4f24ff0132eed2e3086a92828fae0f97f
RJCD00 <rowir052@gmail.com>
2025-10-17 04:27:46 -0500

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
  // app.js
+app.get('/', (req, res) => {
+  res.send('Backend Express funcionando correctamente');
+});
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
